0abffe8f550b7899e3b8d3a9fefda530827eabcf
suhasChintalapati <suhasvarma.ch@gmail.com>
2024-04-15 22:10:28 +0530

    updated code

--- PatientMonitoringSystem/Program.cs
+++ PatientMonitoringSystem/Program.cs
@@ -29,14 +29,14 @@ builder.Services.AddCors(options =>
         );
 });
 //DotENV Configuration
-DotNetEnv.Env.Load();
-CreateHostBuilder(args).Build().Run();
-static IHostBuilder CreateHostBuilder(string[] args) =>
-       Host.CreateDefaultBuilder(args)
-           .ConfigureWebHostDefaults(webBuilder =>
-           {
-               webBuilder.UseStartup<Program>();
-           });
+//DotNetEnv.Env.Load();
+//CreateHostBuilder(args).Build().Run();
+//static IHostBuilder CreateHostBuilder(string[] args) =>
+//       Host.CreateDefaultBuilder(args)
+//           .ConfigureWebHostDefaults(webBuilder =>
+//           {
+//               webBuilder.UseStartup<Program>();
+//           });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
